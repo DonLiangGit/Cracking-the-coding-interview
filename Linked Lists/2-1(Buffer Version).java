39cdc7a94e36ddb193f73626c89eae8b928d6cff
Don Liang <duolingl@gmail.com>
2015-01-10 19:46:23 -0800

    2-1

---
+++ Linked Lists/2-1(Buffer Version).java
@@ -0,0 +1,29 @@
+/* Cracking the coding interview
+ * 2-1 
+ * Remove duplicates from linked list
+ * Use Temporary Buffer(Hash Table)
+ */
+
+public static void removeDuplicates(LinkedListNode node) {
+
+	Hashtable hashtable = new Hashtable();
+	LinkedListNode pre = null;
+	while(node != null) {
+		if(hashtable.containsKey(node.data)) {
+			pre.next = node.next
+		} else {
+			hashtable.put(node.data, true);
+			pre.next = node;
+		}
+		node = node.next;
+	}
+
+}
+
+/*
+ * Extension:
+ * APIs: 
+ * java.util.Hashtable.containsKey()
+ * java.util.Hashtable.put()
+ * Note: 
+ */
