486ba0c084559b82450ec6b3cc33f258b568d860
Don Liang <duolingl@gmail.com>
2015-01-25 20:26:56 -0800

    add path sum with recurisve DFS method

---
+++ Trees and Graphs/Path Sum.java
@@ -0,0 +1,36 @@
+/* Cracking the coding interview
+ * Pre-work 
+ * Path Sum
+ *
+ * Method: DFS
+ */
+
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) {
+            return false;
+        }
+
+        if(root.left == null && root.right == null && sum == root.val) {
+            return true;
+        }
+
+        return hasPathSum(root.left, sum - root.val) || 
+    }
+}
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/*
+ * Extension:
+ * APIs: 
+ * Note: 
+ */
