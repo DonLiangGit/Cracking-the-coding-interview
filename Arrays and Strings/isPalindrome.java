f85387f7c27e6c6c55b7c559eae8fe712186c99a
Don Liang <duolingl@gmail.com>
2015-01-27 16:27:44 -0800

    isPalindrome

---
+++ Arrays and Strings/isPalindrome.java
@@ -0,0 +1,32 @@
+/* Cracking the coding interview
+ * Pre-work 
+ * Valid Palindrome
+ *
+ * Method: two pointers
+ */
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == null || s.length() == 0) {
+            return true;
+        }
+        if(s.length() == 1) {return true;}
+        
+        String newStr = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        if(newStr.length() <= 1) {return true;}
+        
+        for(int i = 0; i < newStr.length(); i++) {
+            if(newStr.charAt(i) != newStr.charAt(newStr.length() - i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+/*
+ * Extension:
+ * APIs: 
+ * Note: 
+ */
