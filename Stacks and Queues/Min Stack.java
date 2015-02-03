c43d0e973dc05cb57be57673b42b2e2d784d2c17
Don Liang <duolingl@gmail.com>
2015-02-02 21:27:50 -0800

    Min Stack

---
+++ Stacks and Queues/Min Stack.java
@@ -0,0 +1,40 @@
+/* Cracking the coding interview
+ * Pre-work 
+ * Min Stack
+ *
+ * Method: 
+ */
+
+class MinStack {
+
+    Stack<Integer> stack = new Stack<Integer>();
+    Stack<Integer> minStack = new Stack<Integer>();
+
+    public void push(int x) {
+        if(minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if(stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/*
+ * Extension:
+ * APIs: 
+ * Note: 
+ */
