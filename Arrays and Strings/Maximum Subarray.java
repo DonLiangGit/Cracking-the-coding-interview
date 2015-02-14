a730903fef0ff40bfd2ab72efe993eb56bc6a561
Don Liang <duolingl@gmail.com>
2015-02-13 16:52:50 -0800

    add maximum subarray

---
+++ Arrays and Strings/Maximum Subarray.java
@@ -0,0 +1,29 @@
+/* Cracking the coding interview
+ * Pre-work 
+ * Maximum Subarray
+ *
+ * Method: 
+ */
+
+public class Solution {
+    public int maxSubArray(int[] A) {
+        if(A == null) {return;}
+        
+        int max = A[0];
+        int[] sum = new int[A.length];
+        sum[0] = A[0];
+        
+        for(int i = 1; i < A.length; i++) {
+            sum[i] = Math.max(A[i], sum[i-1] + A[i]);
+            max = Math.max(max, sum[i]);
+        }
+
+        return max;
+    }
+}
+
+/*
+ * Extension:
+ * APIs: 
+ * Note: 
+ */
