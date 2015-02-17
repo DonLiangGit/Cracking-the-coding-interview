497430bc633f25bb8294663c7fa5462aac711117
Don Liang <duolingl@gmail.com>
2015-02-16 20:02:59 -0800

    add btss iii

---
+++ Arrays and Strings/Best Time to Sell Stock III.java
@@ -0,0 +1,45 @@
+/* Cracking the coding interview
+ * Pre-work 
+ * Best Time to buy and sell stock III
+ *
+ * Method:
+ */
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0; 
+        }
+        
+        int[] left = new int[prices.length];
+        int[] right = new int[prices.length];
+ 
+        // DP from left to right
+        left[0] = 0; // DP initial state
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            left[i] = Math.max(left[i - 1], prices[i] - min); 
+        }
+        
+        // DP from right to left
+        right[prices.length - 1] = 0;  // DP initial state
+        int max = prices[prices.length - 1];
+        for (int i = prices.length - 2; i >= 0; i--) {
+            max = Math.max(max, prices[i]);
+            right[i] = Math.max(right[i + 1], max - prices[i]);
+        }
+        
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            profit = Math.max(profit, left[i] + right[i]);
+        }       
+        return profit;
+    }
+}
+
+/*
+ * Extension:
+ * APIs: 
+ * Note: 
+ */
