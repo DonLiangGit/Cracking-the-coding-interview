0452a8e3729a3a66d3b53a86d58725d4d3830dd6
Don Liang <duolingl@gmail.com>
2015-02-20 21:27:55 -0800

    add Factorial Trailing Zeros here

---
+++ Maths and Probability/Factorial Trailing Zeros.java
@@ -0,0 +1,22 @@
+/* Cracking the coding interview
+ * Pre-work 
+ * Factorial Trailing Zeros
+ *
+ * Method: 
+ */
+
+public class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while(n > 0) {
+            count += n/5;
+            n = n/5;
+        }
+        return count;
+    }
+}
+/*
+ * Extension:
+ * APIs: 
+ * Note: 
+ */
