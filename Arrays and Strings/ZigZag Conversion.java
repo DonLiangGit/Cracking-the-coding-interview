25539a9c64f8b68efbebcda58a0785d547e878d0
Don Liang <duolingl@gmail.com>
2015-02-22 18:11:45 -0800

    add ZigZag Conversion

---
+++ Arrays and Strings/ZigZag Conversion.java
@@ -0,0 +1,44 @@
+/* Cracking the coding interview
+ * Pre-work 
+ * ZigZag Conversion
+ *
+ * Method:
+ */
+
+public class Solution {
+    public String convert(String s, int nRows) {
+        if(nRows == 1) { return s; }
+        StringBuilder sB = new StringBuilder();
+        // Step Formula
+        int step = 2 * nRows - 2;
+        for(int i = 0; i < nRows; i++) {
+            if(i == 0 || i == nRows - 1) {
+                for(int j = i; j < s.length(); j = j + step) {
+                    sB.append(s.charAt(j));
+                }
+            } else {
+                int j = i;
+                boolean flag = true;
+                int step1 = 2 * (nRows - 1 - i);
+                int step2 = step - step1;
+                while (j < s.length()) {
+                    builder.append(s.charAt(j));
+                    if (flag)
+                        j = j + step1;
+                    else
+                        j = j + step2;
+                    flag = !flag;
+                }
+            }
+        }
+
+        return sB.toString();
+    }
+}
+
+/*
+ * Extension:
+ * APIs: 
+ * Note:
+ * input: 
+ */
