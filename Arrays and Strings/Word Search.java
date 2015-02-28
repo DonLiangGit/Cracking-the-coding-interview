afa46c292569370e95edb30072f8fbfd9a7cdde8
Don Liang <duolingl@gmail.com>
2015-02-28 15:41:42 -0800

    update word search

--- Arrays and Strings/Word Search.java
+++ Arrays and Strings/Word Search.java
@@ -15,7 +15,7 @@ public class Solution {
         boolean[][] visited = new boolean[board.length][board[0].length];
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length; j++) {
-                if(search(board, word, 0, i, j, used)) {
+                if(search(board, word, 0, i, j, visited)) {
                     return true;
                 }
             }
@@ -26,7 +26,7 @@ public class Solution {
     private boolean search(char[][] board, String word, int index, int i, int j, boolean[][] used) {
         if(index == word.length()) return true;
 
-        if(i < 0 || j < 0 || i >= board.length || j >= borad[0].length
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length
             || used[i][j] || board[i][j] != word.charAt(index)) {
             return false;
         }
@@ -34,7 +34,7 @@ public class Solution {
         boolean res = search(board, word, index+1, i-1, j, used)
                 || search(board, word, index+1, i+1, j, used)
                 || search(board, word, index+1, i, j-1, used)
-                || search(board, word, index+1, i, j+1, used)
+                || search(board, word, index+1, i, j+1, used);
         used[i][j] = false;
         return res;
     }
